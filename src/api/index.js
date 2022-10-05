10c0258e6a4da3dfc4e2d3d533c2f59e7b61d206
Jacob Morris <104042611+jmofrdao@users.noreply.github.com>
2022-10-05 00:23:07 -0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,4 @@
-const BASE = 'https://www.lottery.ok.gov/plays.json'
 
-//  export async function getGames() {
-//     try {
-//       const response = await fetch(`${BASE}`, {
-//         method: 'GET',
-//         headers: {
-//             "Content-Type": "application/json",
-//           'Access-Control-Allow-Origin': '*'
-//         },
-//       });
-  
-//       if (!response.ok) {
-//         throw new Error(`Error! status: ${response.status}`);
-//       }
-  
-//       const result = await response.json();
-//       return result;
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
 
 export async function getGames () {
     const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.lottery.ok.gov/plays.json')}`)
